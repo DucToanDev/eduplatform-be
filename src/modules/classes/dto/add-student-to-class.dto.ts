2448162361424d1278202e9982ee0431bdcd062c
Your Name <email@example.com>
2026-06-30 00:26:12 +0700

    feat: Add parent access code and add student by email

--- src/modules/classes/dto/add-student-to-class.dto.ts
+++ src/modules/classes/dto/add-student-to-class.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsOptional, IsString, IsEmail } from 'class-validator';
 
 export class AddStudentToClassDto {
-  @ApiProperty({ example: '6659f9f7c1e9e7f0c4f0d111' })
+  @ApiProperty({ example: '6659f9f7c1e9e7f0c4f0d111', required: false })
+  @IsOptional()
   @IsString()
-  @IsNotEmpty()
-  student_id: string;
+  student_id?: string;
+
+  @ApiProperty({ example: 'student@example.com', required: false })
+  @IsOptional()
+  @IsEmail()
+  email?: string;
 }
