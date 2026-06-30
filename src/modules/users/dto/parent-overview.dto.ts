342c1ec8e5e8813cbde46f89409714a599caf895
Your Name <email@example.com>
2026-06-30 20:27:34 +0700

    docs: update swagger example for parent access code

--- src/modules/users/dto/parent-overview.dto.ts
+++ src/modules/users/dto/parent-overview.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ParentOverviewRequestDto {
-  @ApiProperty({ example: 'PHHS1234', description: 'Mã truy cập dành cho phụ huynh' })
+  @ApiProperty({ example: 'PHho2805', description: 'Mã truy cập dành cho phụ huynh' })
   @IsString()
   @IsNotEmpty()
   parent_access_code: string;
